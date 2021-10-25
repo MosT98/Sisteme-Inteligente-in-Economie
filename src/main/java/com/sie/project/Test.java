927a06385567a771ae52080d95c22b3b90069cb8
Andrei Halauca <ahalauca@gmail.com>
2021-10-25 23:09:39 +0300

    Added new rules & fixed formatting

--- src/main/java/com/sie/project/Test.java
+++ src/main/java/com/sie/project/Test.java
@@ -25,27 +25,42 @@ public class Test {
         KieContainer kC = kS.getKieClasspathContainer();
         KieSession kSession = kC.newKieSession("ksession-rules");
 
-//        TargetAudience targetAudience = new TargetAudience(MonthlyIncomeInterval.UNDER_1000);
-//        TargetAudience targetAudience2 = new TargetAudience(AgeInterval.UNDER_18);
-//        SocialMediaProfile socialMediaProfile = new SocialMediaProfile(
-//                "PaginaFacebook", 215, 24, SocialMediaPlatform.FACEBOOK);
-        var promotedProduct = new PromotedProduct(ProductCategory.UNKNOWN, Instant.parse("2020-10-01T00:00:00.00Z"), "NumeProdusPromovat");
-        SocialMediaProfile socialMediaProfile2 = new SocialMediaProfile(
-                "PaginaTest", 215, 24, SocialMediaPlatform.INSTAGRAM);
-//        var promoter = new Promoter(
-//                "Cosmin",
-//                Collections.emptyList(),
-//                List.of(socialMediaProfile2),
-//                Instant.parse("2021-10-01T00:00:00.00Z"),
-//                Instant.parse("2021-10-23T00:00:00.00Z"));
+        var targetAudience = new TargetAudience(MonthlyIncomeInterval.UNDER_1000);
+        var targetAudience2 = new TargetAudience(AgeInterval.UNDER_18);
 
-//        kSession.insert(targetAudience);
-//        kSession.insert(targetAudience2);
-//        kSession.insert(socialMediaProfile);
+        var promotedProduct = new PromotedProduct(ProductCategory.UNKNOWN, Instant.parse("2021-11-20T00:00:00.00Z"), "NumeProdusPromovat");
+        var promotedProduct2 = new PromotedProduct(ProductCategory.UNKNOWN, Instant.parse("2021-10-23T00:00:00.00Z"), "Nutella");
 
+        var socialMediaProfile = new SocialMediaProfile("PaginaTest", 215, 24, SocialMediaPlatform.FACEBOOK);
+        var socialMediaProfile2 = new SocialMediaProfile("PaginaTest1", 200, 20, SocialMediaPlatform.INSTAGRAM);
+        var socialMediaProfile3 = new SocialMediaProfile("PaginaTwitter", 200, 20, SocialMediaPlatform.TWITTER);
+
+        var promoter = new Promoter(
+                "Cosmin",
+                List.of(promotedProduct2),
+                List.of(socialMediaProfile2),
+                Instant.parse("2021-10-26T00:00:00.00Z"),
+                Instant.parse("2021-10-30T00:00:00.00Z"));
+
+        var promoter2 = new Promoter(
+                "AAAA",
+                List.of(promotedProduct2),
+                List.of(socialMediaProfile3),
+                Instant.parse("2021-12-26T00:00:00.00Z"),
+                Instant.parse("2021-12-30T00:00:00.00Z"));
+
+
+
+        kSession.insert(targetAudience);
+        kSession.insert(targetAudience2);
+        kSession.insert(socialMediaProfile);
+        kSession.insert(socialMediaProfile2);
+        kSession.insert(socialMediaProfile3);
         kSession.insert(promotedProduct);
-//        kSession.insert(socialMediaProfile2);
-//        kSession.insert(promoter);
+        kSession.insert(promotedProduct2);
+
+        kSession.insert(promoter);
+        kSession.insert(promoter2);
 
         kSession.fireAllRules();
 
